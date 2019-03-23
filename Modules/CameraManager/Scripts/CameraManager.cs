72bb5905dd94b06c4a7678636273b6f342366b4a
F. Rick Reich <frickreich@gmail.com>
2019-03-23 15:03:52 +0100

    Temporarily Deactivated cameraZoom.

--- Modules/CameraManager/Scripts/CameraManager.cs
+++ Modules/CameraManager/Scripts/CameraManager.cs
@@ -12,7 +12,9 @@ namespace Game.Modules.CameraManager
         private CinemachineVirtualCamera virtualCamera;
         private CinemachineFollowZoom virtualCameraZoom;
 
-        private GameObject player;
+        private Managers.GameManager gameManager;
+
+        private GameObject target;
         private Transform cameraPoint;
         private float zoomAmount;
         private float initialZoom = 15f;
@@ -24,10 +26,7 @@ namespace Game.Modules.CameraManager
         {
             virtualCamera = GetComponentInChildren<CinemachineVirtualCamera>();
             virtualCameraZoom = GetComponentInChildren<CinemachineFollowZoom>();
-            player = GameObject.FindGameObjectWithTag("Player");
-            cameraPoint = player.gameObject.transform.Find("CameraPointController/CameraPoint");
-
-            SetCameraFollowObject(cameraPoint);
+            gameManager = FindObjectOfType<Managers.GameManager>();
         }
 
         void SetCameraFollowObject(Transform followObject)
@@ -38,7 +37,7 @@ namespace Game.Modules.CameraManager
 
         private void FixedUpdate()
         {
-            zoomAmount = player.GetComponent<PlayerController.PlayerCharacterController>().cameraPointPos;
+            //zoomAmount = target.GetComponent<PlayerController.PlayerCharacterController>().cameraPointPos;
 
             SetCameraZoom(zoomAmount);
         }
@@ -47,6 +46,20 @@ namespace Game.Modules.CameraManager
         {
             virtualCameraZoom.m_Width = Mathf.Clamp(initialZoom + (zoomAmount / 2), initialZoom, maxZoom);
             virtualCameraZoom.m_Damping = dampingRate;
+        }
+
+        public void AttachCameraToCar()
+        {
+            target = gameManager.playerCar.gameObject;
+            cameraPoint = target.transform.Find("CameraPointController/CameraPoint");
+            SetCameraFollowObject(cameraPoint);
+        }
+
+        public void AttachCameraToPlayer()
+        {
+            target = gameManager.playerCharacter.gameObject;
+            cameraPoint = target.transform.Find("CameraPointController/CameraPoint");
+            SetCameraFollowObject(cameraPoint);
         }
     }
 }
