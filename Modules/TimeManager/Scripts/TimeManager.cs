ff1752f30d0651160418525d6dedd5b5adbfcf32
F. Rick Reich <frickreich@gmail.com>
2019-03-23 18:47:46 +0100

    Added TimeManager Module.

---
+++ Modules/TimeManager/Scripts/TimeManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Game.Modules.Managers
+{
+	public class TimeManager : MonoBehaviour
+	{
+		public float slowDownFactor = 0.05f;
+		public float slowDownLength = 2.0f;
+
+		private void Update()
+		{
+			Time.timeScale += (1f / slowDownLength) * Time.unscaledDeltaTime;
+			Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+		}
+
+		void StartTimeSlowDown()
+		{
+			Time.timeScale = slowDownFactor;
+			Time.fixedDeltaTime = Time.timeScale * .2f;
+		}
+	}
+}
