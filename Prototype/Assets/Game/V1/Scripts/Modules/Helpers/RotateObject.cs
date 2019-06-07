7109347fa3aabb6b6a95bfc9d63b3c99a0d2ae14
F. Rick Reich <frickreich@gmail.com>
2019-06-07 18:14:52 +0200

    Added rotation helper.

---
+++ Prototype/Assets/Game/V1/Scripts/Modules/Helpers/RotateObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.V1.Helper
+{
+	public class RotateObject : MonoBehaviour
+	{
+		public float rotateX;
+        public float rotateY;
+        public float rotateZ;
+
+        public bool active = false;
+
+		// Update is called once per frame
+		void Update()
+		{
+            if(active)
+            {
+                Rotate(rotateX, rotateY, rotateZ);
+            }
+		}
+
+        public void Rotate(float rotX, float rotY, float rotZ)
+        {   
+            gameObject.transform.Rotate(rotX * Time.deltaTime, rotY * Time.deltaTime, rotZ * Time.deltaTime, Space.Self);
+        }
+	}
+}
